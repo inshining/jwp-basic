3766e452efc32e78a803741fedb539135a922583
inshining <inshining.kim@gmail.com>
2024-04-24 17:36:34 +0900

    🍀[FEAT] : 6.1.5 완료

---
+++ src/main/java/next/web/LoginUserServlet.java
@@ -0,0 +1,33 @@
+package next.web;
+
+import core.db.DataBase;
+import next.model.User;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/user/login")
+public class LoginUserServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        String userId = req.getParameter("userId");
+        String password = req.getParameter("password");
+        User user = DataBase.findUserById(userId);
+        if(user == null){
+            res.sendRedirect("/user/login_failed.html");
+            return;
+        }
+        if(user.getPassword().equals(password)){
+            HttpSession session = req.getSession();
+            session.setAttribute("user", user);
+            res.sendRedirect("/index.html");
+        }else{
+            res.sendRedirect("/user/login_failed.html");
+        }
+    }
+}
